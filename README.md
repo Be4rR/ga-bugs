5d1419c2bcf715d2b6e9a2c037082f0a5cea0a3c
bear_montblanc <44293565+Be4rR@users.noreply.github.com>
2020-05-10 16:24:13 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Test Project
-Based on [Strangerfy](https://github.com/lee2sman/Strangerfy).
+# GA Bugs
--- sketch.js
+++ sketch.js
@@ -1,25 +1,105 @@
-var cnv;
+let cnv;
+
+
+let vehicles = [];
+let food = [];
+let poison = [];
+
+let foodPlot = [];
 
 function setup() {
-	// cnv = createCanvas(100, 100);
 	cnv = createCanvas(600, 500);
 	cnv.parent('canvas');
-	// centerCanvas();
-	// background(51);
 	background(126);
-	noStroke();
+	frameRate(30);
+
+	for (let i = 0; i < 50; i++) {
+		let x = random(width);
+		let y = random(height);
+		vehicles[i] = new Vehicle(x, y);
+	}
+
+	for (let i = 0; i < 30; i++) {
+		let x = random(width);
+		let y = random(height);
+		food.push(createVector(x, y));
+	}
+
+	for (let i = 0; i < 15; i++) {
+		let x = random(width);
+		let y = random(height);
+		poison.push(createVector(x, y));
+	}
 }
 
 function draw() {
-	let x = mouseX;
-	let y = mouseY;
-	let ix = width - mouseX;  // Inverse X
-	let iy = height - mouseY; // Inverse Y
 	background(126);
-	fill(255, 150);
-	ellipse(x, height / 2, y, y);
-	fill(0, 159);
-	ellipse(ix, height / 2, iy, iy);
+
+	if (random(1) < 0.05) {
+		let x = random(width);
+		let y = random(height);
+		food.push(createVector(x, y));
+	}
+
+	if (random(1) < 0.01) {
+		let x = random(width);
+		let y = random(height);
+		poison.push(createVector(x, y));
+	}
+
+	//   let target = createVector(mouseX, mouseY);
+
+	//   //Draw an ellipse at the mouse position
+	//   fill(127);
+	//   stroke(200);
+	//   strokeWeight(2);
+	//   ellipse(target.x, target.y, 48, 48);
+
+	for (let i = 0; i < food.length; i++) {
+		fill(0, 255, 0);
+		noStroke();
+		ellipse(food[i].x, food[i].y, 4, 4);
+	}
+
+	for (let i = 0; i < poison.length; i++) {
+		fill(255, 0, 0);
+		noStroke();
+		ellipse(poison[i].x, poison[i].y, 4, 4);
+	}
+
+
+	// Call the appropriate steering behaviors for our agents
+
+	for (let i = vehicles.length - 1; i >= 0; i--) {
+		vehicles[i].boundaries();
+		vehicles[i].behaviors(food, poison);
+		// v.seek(food);
+		vehicles[i].update();
+		vehicles[i].display();
+
+		var newVehicle = vehicles[i].clone();
+		if (newVehicle != null) {
+			vehicles.push(newVehicle);
+		}
+
+		if (vehicles[i].dead()) {
+			let x = vehicles[i].position.x;
+			let y = vehicles[i].position.y;
+			food.push(createVector(x, y));
+			vehicles.splice(i, 1);
+		}
+
+
+	}
+
+	// print(food.length);
+	foodPlot.unshift(food.length);
+	if (foodPlot.length > 200) {
+		foodPlot.pop();
+	}
+
+	// plotter(foodPlot, width/2+350, height/2+210, "food");
+
 }
 
 
